348a8edab5a680ce246085c8dab267bed2b77f93
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-05-05 21:21:24 +0530

    home page design underway

--- application/views/flat/1-ಮುಖಪುಟ/index.php
+++ application/views/flat/1-ಮುಖಪುಟ/index.php
@@ -7,43 +7,52 @@
 		</div>
 	</div>
 	<div class="row justify-content-between samakalina">
-		<div class="col-md-2">
-			<img class="img-fluid" src="<?=PUBLIC_URL?>images/samakalina.png" />
-		</div>
-		<div class="col-md-4">
-			<div class="card card-big">
+		<div class="col-md-3">
+			<div class="card card-medium">
 				<img class="card-img-top" src="<?=PUBLIC_URL?>images/samakalina/art1.jpg" />
 				<div class="card-body">
-					<h5 class="card-title">ಪ್ರತಿಮೆಗಳ ರಾಜಕಾರಣ</h5>
+					<h5 class="card-title"><strong class="green">ಸಮಕಾಲೀನ</strong><br /> ಪ್ರತಿಮೆಗಳ ರಾಜಕಾರಣ</h5>
 					<p class="card-text">ಪ್ರತಿಮೆಗಳಿಗೆ  ಅರ್ಥಬರುವುದು;  ಅವು ಪ್ರತಿನಿಧಿಸುವ  ತತ್ವವನ್ನು ಸಮಾಜ  ಬದುಕುವುದರಲ್ಲಿ.  ಈ ತತ್ವಬದುಕುವಿಕೆ  ಪ್ರತಿಮೆಗಳಿಲ್ಲದೆಯೂ  ಸಾಧ್ಯವಾಗಬೇಕು.  ತತ್ವಾಚರಣೆಯಿಲ್ಲದ  ಪ್ರತಿಮಾ  ಆರಾಧನೆಯನ್ನು  ಝೆನ್ ಮುಂತಾದ  ಬೌದ್ಧ ದಾರ್ಶನಿಕ  ಶಾಖೆಗಳು ತೀವ್ರವಾಗಿ  ನಿರಾಕರಿಸಿದವು.</p>
 					<p class="card-author">ರಹಮತ್ ತರೀಕೆರೆ</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card card-small">
-				<div class="card-body">
-					<h5 class="card-title">ಉತ್ತರ ಪ್ರದೇಶ ಮತ್ತು ಬಿಹಾರದ ಫಲಿತಾಂಶಗಳು ಏನು ಹೇಳುತ್ತಿವೆ?</h5>
-					<p class="card-text">ಇತ್ತೀಚಿಗೆ ಕಾಂಗ್ರೆಸ್ಸಿನ ಭ್ರಷ್ಟಾಚಾರಕ್ಕಿಂತ  ಬಿಜೆಪಿಯ ಕೋಮುವಾದ ಸಹನೀಯ  ಅನ್ನಿಸತೊಡಗಿದೆ ಎಂಬುದರಿಂದಲಾದರೂ  ಸೆಕ್ಯುಲರ್‍ವಾದಿಗಳು ಪಾಠ  ಕಲಿಯಬೇಡವೇ? ಆದರೆ ಪ್ರಜಾಪ್ರಭುತ್ವ,  ಧರ್ಮನಿರಪೇಕ್ಷತೆ ಮತ್ತು ಒಳಗೊಳ್ಳುವ  ರಾಜಕಾರಣದ ತತ್ವಗಳ ದೀರ್ಘ  ಪರಂಪರೆಯುಳ್ಳ ಕಾಂಗ್ರೆಸ್‍ನಂತಹ  ರಾಷ್ಟ್ರೀಯ ಪಕ್ಷವೊಂದು ಕೋಮುವಾದಿ  ವಿರೋಧಿ ಹೋರಾಟದ ಇಂತಹ  ಬೋಳೆತನಕ್ಕೆ ಸಿಕ್ಕಿ ನಿರ್ನಾಮವಾಗಲು  ಬಿಡಬೇಕೆ?</p>
-					<p class="card-author">ಡಿ.ಎಸ್.ನಾಗಭೂಷಣ</p>
 				</div>
 			</div>
 			<div class="card card-medium">
 				<img class="card-img-top" src="<?=PUBLIC_URL?>images/samakalina/art2.jpg" />
 				<div class="card-body">
-					<h5 class="card-title">ಸಿದ್ದರಾಮಯ್ಯನವರ ಕಲ್ಯಾಣ ರಾಜ್ಯ !</h5>
+					<h5 class="card-title"><strong class="green">ಸಮಕಾಲೀನ</strong><br /> ಸಿದ್ದರಾಮಯ್ಯನವರ ಕಲ್ಯಾಣ ರಾಜ್ಯ !</h5>
 					<p class="card-text">ಹಿಂದೊಮ್ಮೆ ನಾನು ಲಂಕೇಶ್ ಪತ್ರಿಕೆಯಲ್ಲಿದ್ದಾಗ ಮೇಷ್ಟ್ರು  ಲಂಕೇಶರ ಜೊತೆ ಚುನಾವಣಾ ವರದಿ ಮಾಡಲು ಕನಕಪುರದ  ಹಳ್ಳಿಗಳ ಕಡೆಗೆ ಹೋಗಿದ್ದೆ.</p>
 					<p class="card-author">ಹನುಮಂತರೆಡ್ಡಿ ಶಿರೂರು</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-3">
-			<div class="card card-small">
-				<img class="card-img-top" src="<?=PUBLIC_URL?>images/samakalina/art4.jpg" />
+		<div class="col-md-6">
+			<div class="card card-big">
 				<div class="card-body">
-					<h5 class="card-title">ರೂಪಕಗಳ ಪರಿಧಿಯನ್ನು ಮೀರಿದ ಚೈತನ್ಯ ಸ್ಟೀಫನ್ ಹಾಕಿಂಗ್</h5>
-					<p class="card-text">ಹಾಕಿಂಗ್‍ರ ಕರ್ಮಭೂಮಿ ಕೇಂಬ್ರಿಡ್ಜ್ ವಿಶ್ವವಿದ್ಯಾಲಯದಲ್ಲೇ ಸಂಶೋಧನೆ ಮಾಡಿದ,  ಅವರನ್ನು ಸ್ವತಃ ಕಂಡು ರೋಮಾಂಚನಗೊಂಡ ಸಹಾಯಕ ಪ್ರಾಧ್ಯಾಪಕಿ ಸುಧಾ ಶೆಣೈ  ಅವರು ಅಗಲಿದ ಚೇತನದ ಜೀವನ, ವ್ಯಕ್ತಿತ್ವ ಹಾಗೂ ಸಾಧನೆಗಳನ್ನಿಲ್ಲಿ ಬಿಚ್ಚಿಟ್ಟಿದ್ದಾರೆ.</p>
-					<p class="card-author">ಸುಧಾ ಶೆಣೈ</p>
+					<h1 class="card-title-big orange">ಚುನಾವಣೆ: ಯಾರ ಹೊಣೆ?</h1>
+				</div>
+				<img class="card-img-top" src="<?=PUBLIC_URL?>images/mukhyacharche/art8.jpg" />
+				<div class="card-body">
+					<h5 class="card-title">ನಮ್ಮ ಬೆರಳ ತುದಿಯಲ್ಲಿ ನಾಡಿನ ಭವಿಷ್ಯ</h5>
+					<p class="card-text">ನಮ್ಮ ರಾಜಕೀಯ ವ್ಯವಸ್ಥೆ ಎಷ್ಟೇ ಹದಗೆಟ್ಟಿದ್ದರೂ, ನಮ್ಮ ರಾಜಕಾರಣಿಗಳು ಎಷ್ಟೇ ಭ್ರಷ್ಟರಾಗಿದ್ದರೂ, ನೋಟು ಪಡೆದು ವೋಟು ನೀಡುವ ಮನೋಭಾವ ಹೊಂದಿದ ಮತದಾರರು ಪ್ರಜಾಪ್ರಭುತ್ವ ವ್ಯವಸ್ಥೆಗೇ ಮಾರಕ.</p>
+					<p class="card-category"><strong class="orange">ಮುಖ್ಯಚರ್ಚೆ</strong></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="card card-medium">
+				<img class="card-img-top" src="<?=PUBLIC_URL?>images/jagadarivu/art1.jpg" />
+				<div class="card-body">
+					<h5 class="card-title"><strong class="blue">ಜಗದರಿವು</strong><br /> ಡೊನಾಲ್ಡ್ ಟ್ರಂಪ್ ಮತ್ತು ಅಮೆರಿಕಾದ ವರ್ಣೀಯ ರಾಜಕಾರಣ</h5>
+					<p class="card-text">ಅಮೆರಿಕಾದ   ರಾಜಕಾರಣದಲ್ಲಿ   ಶ್ವೇತವರ್ಣೀಯರ   ಪ್ರಾಬಲ್ಯವೆನ್ನುವುದು   ಒಂದು ಜೀವಂತ   ಚೇತನವಾಗಿ ಅದರ   ಇತಿಹಾಸದುದ್ದಕ್ಕೂ   ಉಳಿದುಬಂದಿದೆ.   ಟ್ರಂಪ್ ಅಂತಹ   ಚೇತನದಿಂದ   ಹೊರಹೊಮ್ಮಿರುವ   ರಾಜಕಾರಣಿ.</p>
+					<p class="card-author">ಪ್ರೊ. ಕ್ರಿಸ್ಟೋಫರ್ ಚೆಕೂರಿ</p>
+				</div>
+			</div>
+			<div class="card card-medium">
+				<img class="card-img-top" src="<?=PUBLIC_URL?>images/samskriti/art1.jpg" />
+				<div class="card-body">
+					<h5 class="card-title"><strong class="red">ಸಂಸ್ಕೃತಿ ಸಂಪದ</strong><br /> ‘ಗೋತ್ರಾನೂ ಒಂದ ಧೋತ್ರಾನೂ ಒಂದ...’</h5>
+					<p class="card-text">ವಿಶ್ವವಿಖ್ಯಾತ ಸರೋದ್ ವಾದಕ, ಹಿಂದೂಸ್ಥಾನಿ ಸಂಗೀತ ದಿಗ್ಗಜ ರಾಜೀವ ತಾರಾನಾಥ ಅವರ ಅಸಂಪ್ರದಾಯಿಕ ಸಂದರ್ಶನ.</p>
+					<p class="card-author">ಶ್ರೀದೇವಿ ಕಳಸದ</p>
 				</div>
 			</div>
 		</div>
