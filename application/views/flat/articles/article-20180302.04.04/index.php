49c044a0d390586dbb86eb0f176f429c9223fe84
kishorssk <kishor.sathigrama@gmail.com>
2018-05-22 17:50:00 +0530

    articles data and images included

---
+++ application/views/flat/articles/article-20180302.04.04/index.php
@@ -0,0 +1,11 @@
+<div class="container-fluid">
+	<div class="row align-items-top justify-content-center kannada">
+		<div class="col-md-8 article">
+			<p class="date english">2<sup>nd</sup> March 2018</p>
+			<h1 class="title">ಹೊಸ ಪುಸ್ತಕ</h1>
+			
+		
+		</div>
+		<?php require_once(PHY_FLAT_URL . 'article-lists/pustaka.php'); ?>
+	</div>
+</div>
