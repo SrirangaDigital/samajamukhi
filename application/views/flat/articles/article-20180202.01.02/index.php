fd03da7af41f6c95d129d6084eb2754321a1c172
kishorssk <kishor.sathigrama@gmail.com>
2018-05-23 18:11:48 +0530

    articles data and images included

---
+++ application/views/flat/articles/article-20180202.01.02/index.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+	<div class="row align-items-top justify-content-center kannada">
+		<div class="col-md-8 article">
+			<p class="date english">2<sup>nd</sup> March 2018</p>
+			<h1 class="title"></h1>
+			<h2 class="author"></h2>
+			<p class="description"></p>
+
+		</div>
+		<?php require_once(PHY_FLAT_URL . 'article-lists/samakalina.php'); ?>
+	</div>
+</div>
