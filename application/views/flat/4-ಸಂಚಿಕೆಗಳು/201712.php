76fcd0ff3a7bd34154babaece32431f8943bd875
kishorssk <kishor.sathigrama@gmail.com>
2018-06-12 16:31:52 +0530

    Create Sanchike Pages

---
+++ application/views/flat/4-ಸಂಚಿಕೆಗಳು/201712.php
@@ -0,0 +1,169 @@
+<div class="container-fluid">
+	<div class="row align-items-top justify-content-center kannada">
+		<div class="col-md-12 issues">
+			<div class="row issue-toc">
+				<div class="col-md-3">
+					<div class="row align-items-center justify-content-center">
+						<div class="col-md-8">
+							<figure>
+								<img class="img-fluid" src="<?=PUBLIC_URL?>images/cover.jpg" />
+								<figcaption>ಡಿಸೆಂಬರ್ 2017<br />ಸಂಪುಟ , ಸಂಚಿಕೆ</figcaption>
+							</figure>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<h2>ಬುದ್ದಿಗೆ ಬೀಗ?</h2>
+<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಟಿ ಪೃಥ್ವಿದತ್ತ ಚಂದ್ರಶೋಭಿ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.01">ಕನ್ನಡದ ಮನಸ್ಸು ಇಂದು ಮುಚ್ಚುತ್ತಿದೆಯೆ?</a></h5>
+		</div>
+	</div>
+	<h2>ಸಂದರ್ಶನ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.02">ಕನ್ನಡ ಸಂಶೋಧಕ ಷ.ಶೆಟ್ಟರ್</a></h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಸಂದರ್ಶಕ: ಜೋಗಿ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.05">‘ನನ್ನನ್ನು ತಕ್ಷಣ ನಂಬಬೇಡಿ!’ ಪ್ರಕಾಶ್ ರೈ</a></h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಡಾ.ಎನ್.ಎಸ್.ಗುಂಡೂರ</p>
+			<p class="title"><a href=="<?=BASE_URL?>articles/article-20171202.xx.03">ಕನ್ನಡಕ್ಕೆ ಇನ್ನೊಂದು ‘ಕವಿರಾಜಮಾರ್ಗ’</a></h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಗೋವಿಂದ ಬೆಳಗಾಂವ್ಕರ್</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.04">ಒಡೆದ ಮನದಲಿ ಉರಿವ ಕೋಮುಕೆಂಡ!</a></h5>
+		</div>
+	</div>
+	<h2>ನನ್ನ ಓದು</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.06">ಕಡಿಮೆ ಹಣದುಬ್ಬರಕ್ಕೆ ಒಗ್ಗುವ ಸಮಯದಲ್ಲಿ...</a></h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಮೋಹನದಾಸ್</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.07">ವೈಚಾರಿಕ ಬಿಚ್ಚು ಮನಸ್ಸಿಗೆ ಸಾಕ್ಷಿ</a></h5>
+		</div>
+	</div>
+	<h2>ಶಿಕ್ಷಣ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಪ್ರೊ.ಚಿದಾನಂದ ಎಸ್.ಪಾಟೀಲ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.08">ಪ್ರಸಕ್ತ ಕಾನೂನು ಶಿಕ್ಷಣ ಮತ್ತು ವೃತ್ತಿ ಅವಕಾಶ</a></h5>
+		</div>
+	</div>
+	<h2>ವಿವಾದ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಬಸವರಾಜ ಭೂಸಾರೆ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.09">ಬಡ್ತಿ ಮೀಸಲಾತಿ ವಿವಾದ: ಸರಿ ತಪ್ಪುಗಳ ನಡುವಿನ ಸತ್ಯ</a></h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.10">ಆಗುಹೋಗು</a></h5>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+				</div>
+				<div class="col-md-3">
+	<h2>ವಿಡಂಬನೆ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಗುರುಪ್ರಸಾದ ಕುರ್ತಕೋಟಿ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.11">ಸರಣಿ ಪಾತಕಿಗಳು!</a></h5>
+		</div>
+	</div>
+	<h2>ಹೊರನೋಟ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಶ್ರೀಹರ್ಷ ಸಾಲಿಮಠ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.12">ಅನ್ಯ ಭಾಷೆಗಳಿಗೆ ಇಲ್ಲದ ಆತಂಕ ಕನ್ನಡಕ್ಕೇಕೆ?</a></h5>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-12">
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.13">ನನ್ನ ಕ್ಲಿಕ್</a></h5>
+		</div>
+	</div>
+			<h2>ಪರಿಸರ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಸಮದ್ ಕೊಟ್ಟೂರು</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.14">ಕೆಂಪು ಗಣಿಗಳ ಸುತ್ತ ನೀಲಕುರಿಂಜಿ ಉತ್ಸವ</a></h5>
+		</div>
+	</div>
+	<h2>ಹೊಸ ಚರ್ಚೆ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಚಂದ್ರಗಿರಿ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.15">ಗಿರಿಜನರೆಂದರೆ ಕಾಡಿನಲ್ಲಿ ಸಂರಕ್ಷಿಸುವ ಪ್ರಾಣಿಗಳೇ?</a></h5>
+		</div>
+	</div>
+				</div>
+				<div class="col-md-3">
+		
+	<h2>ಹಳಗನ್ನಡ ಕಾವ್ಯ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಪ್ರೊ.ಹೆಚ್.ಜೆ.ಸರಸ್ವತಿ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.16">ಚಾಮರಸನ ಪ್ರಭುಲಿಂಗಲೀಲೆ</a></h5>
+		</div>
+	</div>
+	<h2>ಹಳೇ ಬೇರು</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಎಚ್ ಎಸ್ ಶಿವಪ್ರಕಾಶ್</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.17">ಭಗೀರಥ ಪ್ರಾರ್ಥನೆ</a></h5>
+		</div>
+	</div>
+
+				<h2>ಲೇಖಕ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಎಸ್. ಸಿ. ರಾಜ್ ಅಹಮದ್</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.18">ವ್ಯಕ್ತಿಗಳ ತಲ್ಲಣಗಳ ಮೂಲಕ ರಾಜಕೀಯ ಸಾಮಾಜಿಕ ಪ್ರಶ್ನೆಗಳನ್ನು ಕೇಳುವ ಇಷಿಗುರೊ</a></h5>
+		</div>
+	</div>
+	<h2>ಪ್ರವಾಸ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಅಮರಜಾ ಹೆಗಡೆ</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.19">ಮುಸ್ಲಿಂ ರಾಷ್ಟ್ರದಲ್ಲೊಂದು ಪ್ರಸಿದ್ಧ ಹಿಂದೂ ಗುಹಾಲಯ</a></h5>
+		</div>
+	</div>
+	<h2>ಇತಿಹಾಸ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author">ಮೂಲ: ಜಾಕ್ ವೆದರ್ ಫೋರ್ಡ್ | ಕನ್ನಡಕ್ಕೆ: ಎಸ್.ಸಿರಾಜ್ ಅಹಮದ್</p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.20">ಚೆಂಗೀಸ್ ಖಾನ್</a></h5>
+		</div>
+	</div>
+	<h2>ನೀವೊ ಮಾಡಿ</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author"></p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20171202.xx.21">ಮಾತಿನ ಕಟ್ಟೆಗಳ ಮುಂದಿನ ಮೆಟ್ಟಿಲು</a></h5>
+		</div>
+	</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
