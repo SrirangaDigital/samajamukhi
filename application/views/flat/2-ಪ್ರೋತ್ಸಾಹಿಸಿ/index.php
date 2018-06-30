8c0173bd78dc98a56c36b04cf64da9084f290c95
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-06-30 22:02:49 +0530

    scubscription page and htacces mods

--- application/views/flat/2-ಪ್ರೋತ್ಸಾಹಿಸಿ/index.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid">
-	<div class="row align-items-top justify-content-center kannada">
-		<div class="col-md-11 article issues">
-			<h1 class="title">ಪ್ರೋತ್ಸಾಹಿಸಿ</h1>
-		</div>
-	</div>
-</div>
--- application/views/flat/3-ಚಂದಾದಾರರಾಗಿ/index.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid">
-	<div class="row align-items-top justify-content-center kannada">
-		<div class="col-md-11 article issues">
-			<h1 class="title">ಚಂದಾದಾರರಾಗಿ</h1>
-		</div>
-	</div>
-</div>
