c3b73a55007fdce3362a02a5cfd432ec8922c039
kishorssk <kishor.sathigrama@gmail.com>
2018-07-24 16:47:59 +0530

    links update

--- application/views/flat/article-lists/pratibimba.php
+++ application/views/flat/article-lists/pratibimba.php
@@ -32,4 +32,25 @@
 			<p class="date"><i class="fa fa-book"></i> ಮೇ ೨೦೧೮</p>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author"></p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20180402.06.01">ಮೈಸೂರಿನಲ್ಲಿ ‘ಸಮಾಜಮುಖಿ’ ಸಂವಾದ</a></p>
+			<p class="date"><i class="fa fa-book"></i> ಏಪ್ರಿಲ್ ೨೦೧೮</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author"></p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20180302.06.01">ಧಾರವಾಡದಲ್ಲಿ ‘ಸಮಾಜಮುಖಿ’ ವಿಚಾರ ಸಂಕಿರಣ</a></p>
+			<p class="date"><i class="fa fa-book"></i> ಮಾರ್ಚ್ ೨೦೧೮</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="author"></p>
+			<p class="title"><a href="<?=BASE_URL?>articles/article-20180202.06.01">ಸಮಾಜಮುಖಿ ಲೋಕಾರ್ಪಣೆ</a></p>
+			<p class="date"><i class="fa fa-book"></i> ಫೆಬ್ರವರಿ ೨೦೧೮</p>
+		</div>
+	</div>
 </div>
